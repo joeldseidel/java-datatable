8b42546f788a828157a27007bd5cb7f2d748cf71
Joel Seidel <joeldseidel@gmail.com>
2018-11-30 01:26:45 -0500

    get columns from meta data

--- src/DataTable.java
+++ src/DataTable.java
@@ -1,13 +1,33 @@
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
 import java.util.List;
 
 public class DataTable {
+    private String tableName;
+    private String schemaName;
+    private String catalogName;
     private List<TableColumn> columns;
     private List<TableRow> rows;
 
-    public DataTable(ResultSet resultSet) {
-        //Get table schema
-        //Parse result set to populate rows
+    public DataTable(ResultSet resultSet) throws SQLException {
+        parseTableSchema(resultSet);
+
+    }
+
+    private void parseTableSchema(ResultSet resultSet) throws SQLException {
+        ResultSetMetaData metaData = resultSet.getMetaData();
+        this.tableName = metaData.getTableName(0);
+        this.schemaName = metaData.getSchemaName(0);
+        this.catalogName = metaData.getCatalogName(0);
+        createColumns(metaData);
+    }
+
+    private void createColumns(ResultSetMetaData metaData) throws SQLException{
+        for(int i = 0; i < metaData.getColumnCount(); i++){
+            TableColumn thisColumn = new TableColumn(metaData.getColumnClassName(i), metaData.getColumnDisplaySize(i), metaData.getColumnLabel(i), metaData.getColumnName(i), metaData.getColumnType(i), metaData.getColumnTypeName(i), metaData.getPrecision(i), metaData.getScale(i), metaData.isAutoIncrement(i), metaData.isCaseSensitive(i), metaData.isCurrency(i), metaData.isDefinitelyWritable(i), metaData.isNullable(i), metaData.isReadOnly(i), metaData.isSearchable(i), metaData.isSigned(i), metaData.isWritable(i));
+            this.columns.add(thisColumn);
+        }
     }
 
     private List<TableColumn> getColumns(){
--- src/TableColumn.java
+++ src/TableColumn.java
@@ -1,16 +1,107 @@
-public class TableColumn {
+class TableColumn {
+    private String className;
+    private int displaySize;
+    private String label;
     private String name;
-    private String dataType;
+    private int type;
+    private String typeName;
+    private int precision;
+    private int scale;
+    private boolean isAutoIncrement;
+    private boolean isCaseSensitive;
+    private boolean isCurrency;
+    private boolean isDefinitelyWritable;
+    private int isNullable;
+    private boolean isReadOnly;
+    private boolean isSearchable;
+    private boolean isSigned;
+    private boolean isWritable;
 
-    public TableColumn(String name, String dataType){
+    TableColumn(String className, int displaysize, String label, String name, int type, String typeName, int precision, int scale, boolean isAutoIncrement, boolean isCaseSensitive, boolean isCurrency, boolean isDefinitelyWritable, int isNullable, boolean isReadOnly, boolean isSearchable, boolean isSigned, boolean isWritable){
+        this.className = className;
+        this.displaySize = displaysize;
+        this.label = label;
         this.name = name;
-        this.dataType = dataType;
+        this.type = type;
+        this.typeName = typeName;
+        this.precision = precision;
+        this.scale = scale;
+        this.isAutoIncrement = isAutoIncrement;
+        this.isCaseSensitive = isCaseSensitive;
+        this.isCurrency = isCurrency;
+        this.isDefinitelyWritable = isDefinitelyWritable;
+        this.isNullable = isNullable;
+        this.isReadOnly = isReadOnly;
+        this.isSearchable = isSearchable;
+        this.isSigned = isSigned;
+        this.isWritable = isWritable;
     }
 
-    public String getName(){
-        return this.name;
+    public String getClassName() {
+        return className;
     }
-    public String getDataType(){
-        return this.dataType;
+
+    public int getDisplaySize() {
+        return displaySize;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public String getTypeName() {
+        return typeName;
+    }
+
+    public int getPrecision() {
+        return precision;
+    }
+
+    public int getScale() {
+        return scale;
+    }
+
+    public boolean isAutoIncrement() {
+        return isAutoIncrement;
+    }
+
+    public boolean isCaseSensitive() {
+        return isCaseSensitive;
+    }
+
+    public boolean isCurrency() {
+        return isCurrency;
+    }
+
+    public boolean isDefinitelyWritable() {
+        return isDefinitelyWritable;
+    }
+
+    public int getIsNullable() {
+        return isNullable;
+    }
+
+    public boolean isReadOnly() {
+        return isReadOnly;
+    }
+
+    public boolean isSearchable() {
+        return isSearchable;
+    }
+
+    public boolean isSigned() {
+        return isSigned;
+    }
+
+    public boolean isWritable() {
+        return isWritable;
     }
 }
